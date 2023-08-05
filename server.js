2186ea7940b2c2bb382367bde1867d19fb37ddad
Asif Muhammed <asifasifpsps@gmail.com>
2023-08-05 19:34:08 +0530

    update:server connected

--- server.js
+++ server.js
@@ -20,6 +20,65 @@ app.use(session({
 
 const PORT = process.env.PORT || 3000
 
+const email = "asifasifpsps@gmail.com"
+const password = "123"
+
+
+
+
+app.get("/", checkLogin, (req, res) => {
+    res.render("index", { name: req.session.user.id })
+})
+
+app.use("/", function (err, req, res, next) {
+    console.log(err)
+
+    res.render("login")
+})
+
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+
+app.post("/login", (req, res) => {
+    console.log(req.session)
+    console.log(req.body)
+
+    if (!req.body.email || !req.body.password) {
+   
+        res.render("login", { message: "Please enter both email and password" })
+
+    } else {
+
+        if (user = req.body.email === email && req.body.password === password) {
+
+            req.session.user = user
+            res.redirect("/")
+
+        }
+        res.render("login", { message: "invalid credentials" })
+
+    }
+})
+
+app.get("/logout", (req, res) => {
+    req.session.destroy(function () {
+        console.log("user logged out")
+    })
+    res.redirect("/login")
+})
+
+
+
+function checkLogin(req, res, next) {
+    if (req.session.user) {
+        next()
+    } else {
+        var err = new Error("user not logged in")
+        next(err)
+    }
+}
 
 app.listen(PORT, () => {
     console.log("server is running")
